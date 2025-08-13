e7261afcaa348bc25aa2befd296bca44ac64ba85
ten4i <oleksijchunihin@gmail.com>
2025-08-13 19:40:18 +0300

    Revert "Solution"

--- app/main.py
+++ app/main.py
@@ -1,7 +1,3 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    letter_counter = 0
-    for char in phrase.lower():
-        if char == letter.lower():
-            letter_counter += 1
-
-    return letter_counter
+    # write your code here
+    pass
